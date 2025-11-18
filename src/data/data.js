b4004676f95dfb179a76eecfd63bd922176bfb47
Divil Thakur <divilthakur@gmail.com>
2025-11-19 00:14:52 +0530

    fixes

--- src/data/data.js
+++ src/data/data.js
@@ -1,3 +1,24 @@
+import github from "../assets/images/github.png";
+import mongo from "../assets/images/mongo.png";
+import postgre from "../assets/images/postgre.png";
+import elastic from "../assets/images/elastic.png";
+import cassandra from "../assets/images/cassandra.png";
+import redis from "../assets/images/redis.png";
+import hashicorp from "../assets/images/hashicorp.png";
+import harness from "../assets/images/harness.png";
+import apache from "../assets/images/apache.png";
+import emirates from "../assets/images/emirates.png";
+import roshan from "../assets/images/roshan.png";
+import etisalat from "../assets/images/etisalat.png";
+import zain from "../assets/images/zain.png";
+import rakbank from "../assets/images/rakbank.png";
+import capital from "../assets/images/capital.png";
+import liv from "../assets/images/liv.png";
+import aljazira from "../assets/images/aljazira.png";
+import pic1 from "../assets/images/pic1.png";
+import pic2 from "../assets/images/pic2.png";
+import pic3 from "../assets/images/pic3.png";
+import { li } from "framer-motion/client";
 
 
 const descriptionText = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam. amet, consetetur.";
@@ -6,19 +27,19 @@ const descriptionText = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr
 export const techStack = [
     {
         id: 1,
-        img: "./src/assets/images/github.png",
+        img: { github },
         title: "GitHub",
         desc: descriptionText,
     },
     {
         id: 2,
-        img: "./src/assets/images/mongo.png",
+        img: { mongo },
         title: "Mongo DB",
         desc: descriptionText,
     },
     {
         id: 3,
-        img: "./src/assets/images/postgre.png",
+        img: { postgre },
         title: "Postgre SQL",
         desc: descriptionText,
     },
@@ -28,19 +49,19 @@ export const techStack = [
 export const DataAnalysis = [
     {
         id: 1,
-        img: "./src/assets/images/elastic.png",
+        img: { elastic },
         title: "Elastic",
         desc: descriptionText,
     },
     {
         id: 2,
-        img: "./src/assets/images/cassandra.png",
+        img: { cassandra },
         title: "DataStax Cassandra",
         desc: descriptionText,
     },
     {
         id: 3,
-        img: "./src/assets/images/redis.png",
+        img: { redis },
         title: "Redis",
         desc: descriptionText,
     },
@@ -50,19 +71,19 @@ export const DataAnalysis = [
 export const Technology = [
     {
         id: 1,
-        img: "./src/assets/images/hashicorp.png",
+        img: { hashicorp },
         title: "HashiCorp",
         desc: descriptionText,
     },
     {
         id: 2,
-        img: "./src/assets/images/harness.png",
+        img: { harness },
         title: "Harness",
         desc: descriptionText,
     },
     {
         id: 3,
-        img: "./src/assets/images/apache.png",
+        img: { apache },
         title: "Apache Kafka",
         desc: descriptionText,
     },
@@ -72,33 +93,33 @@ export const Technology = [
 
 
 export const clientsData = [
-    { id: 1, img: "./src/assets/images/emirates.png", alt: "Emirates NBD" },
-    { id: 2, img: "./src/assets/images/roshan.png", alt: "Roshan" },
-    { id: 3, img: "./src/assets/images/etisalat.png", alt: "Etisalat" },
-    { id: 4, img: "./src/assets/images/zain.png", alt: "Zain" },
-    { id: 5, img: "./src/assets/images/rakbank.png", alt: "Rakbank" },
-    { id: 6, img: "./src/assets/images/capital.png", alt: "Capital Bank" },
-    { id: 7, img: "./src/assets/images/liv.png", alt: "Liv" },
-    { id: 8, img: "./src/assets/images/aljazira.png", alt: "Bank Aljazira" },
+    { id: 1, img: { emirates }, alt: "Emirates NBD" },
+    { id: 2, img: { roshan }, alt: "Roshan" },
+    { id: 3, img: { etisalat }, alt: "Etisalat" },
+    { id: 4, img: { zain }, alt: "Zain" },
+    { id: 5, img: { rakbank }, alt: "Rakbank" },
+    { id: 6, img: { capital }, alt: "Capital Bank" },
+    { id: 7, img: { liv }, alt: "Liv" },
+    { id: 8, img: { aljazira }, alt: "Bank Aljazira" },
 ];
 
 
 export const caseStudiesData = [
     {
         id: 1,
-        img: "./src/assets/images/pic1.png",
+        img: { pic1 },
         title: "Social Finance: A Decentralized Creators Economy",
         date: "16 Nov 2021",
     },
     {
         id: 2,
-        img: "./src/assets/images/pic2.png",
+        img: { pic2 },
         title: "Switzerland-Wide Rollout Of Digital Authorization For Unmanned Aer...",
         date: "16 Nov 2021",
     },
     {
         id: 3,
-        img: "./src/assets/images/pic3.png",
+        img: { pic3 },
         title: "Poisson Distribution - Adapting For Different Intervals",
         date: "16 Nov 2021",
     },
